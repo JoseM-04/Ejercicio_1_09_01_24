7a15103b720701648099c7d6a7387705c49160e1
JoseM-04 <150004025+JoseM-04@users.noreply.github.com>
2024-01-09 10:04:04 +0100

    Actualizacion

--- Ejercicio_1_09_01_24/src/Principal/Cine.java
+++ Ejercicio_1_09_01_24/src/Principal/Cine.java
@@ -1,9 +1,12 @@
 package Principal;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Cine {
 
 	public static void main(String[] args) {
 		
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	}
 }
