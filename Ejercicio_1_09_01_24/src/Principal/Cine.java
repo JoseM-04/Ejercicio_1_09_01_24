802da75e06e8ecc6f461b6ebfadc7eda84a4304e
JoseM-04 <150004025+JoseM-04@users.noreply.github.com>
2024-01-09 12:21:30 +0100

    final

--- Ejercicio_1_09_01_24/src/Principal/Cine.java
+++ Ejercicio_1_09_01_24/src/Principal/Cine.java
@@ -1,16 +1,50 @@
 package Principal;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Cine {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int ascientos [][] = new int [10][10];
-		
-		System.out.println();
+    	int filas = 7;
+    	int columnas = 7;
+    	char [][] asientos = new char[filas][columnas];
+    	
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                asientos[i][j] = '0';
+            }
+        }
+    	
+    	while (true) {
+			
+        System.out.print("Ingrese el número de fila (1-" + filas + "): ");
+        int fila = scanner.nextInt();
+
+        System.out.print("Ingrese el número de columna (1-" + columnas + "): ");
+        int columna = scanner.nextInt();
+        
+        
+
+        if (asientos[fila - 1][columna - 1] == '0') {
+            asientos[fila - 1][columna - 1] = 'X'; 
+            System.out.println("¡Asiento reservado con éxito!");
+        } else {
+            System.out.println("¡Asiento ocupado!... Por favor, elija otro.");
+        }
+    	
+    	System.out.println("Actualizacion de los asientos:");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(asientos[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
 	}
+    
+}
+ 
 }
